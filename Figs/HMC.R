0cf0098ab74ec600637a6e7cfbc15bf9d0399e69
FAZH <fazepher@gmail.com>
2019-04-15 12:33:57 -0500

    Inicio HMC

---
+++ Figs/HMC.R
@@ -0,0 +1,42 @@
+library(tidyverse)
+library(magrittr)
+
+rosa <- "#F78D7C"
+azul <- "#4E67C8"
+lucify_basics <- function(){
+  theme(line = element_line(colour = rgb(red = 102, green = 102, blue = 102, maxColorValue = 255)),
+        rect = element_rect(colour = rgb(red = 198, green = 198, blue = 198, maxColorValue = 255)),
+        text = element_text(family = "AvantGarde Bk BT", 
+                            colour = rgb(red = 60, green = 60, blue = 60, maxColorValue = 255),
+                            size = 17),
+        axis.title.x = element_text(size = rel(1.2),
+                                    margin = margin(t = 15)),
+        axis.title.x.top = element_text(size = rel(1.2),
+                                        margin = margin(b = 15)),
+        axis.title.y = element_text(size = rel(1.2),
+                                    margin = margin(r = 30)),
+        axis.title.y.right = element_text(size = rel(1.2),
+                                          margin = margin(l = 30)),
+        axis.text.x = element_text(margin = margin(t = 8)),
+        axis.text.x.top = element_text(margin = margin(b = 8)),
+        axis.text.y = element_text(margin = margin(r = 8)),
+        axis.text.y.right = element_text(margin = margin(l = 8)),
+        axis.ticks = element_blank(),
+        legend.margin = margin(t = 15, r = 15, b = 15, l = 15),
+        legend.title = element_text(size = rel(1.15)),
+        panel.spacing = unit(15,"pt"),
+        plot.title = element_text(size = rel(1.3),
+                                  hjust = 0.5,
+                                  margin = margin(b = 20)),
+        plot.subtitle = element_text(hjust = 0.5, 
+                                     margin = margin(t = -12.5, b = 20)),
+        plot.margin = margin(t = 10, r = 10, b = 10, l = 10),
+        strip.text = element_text(size = rel(1.1),
+                                  margin = margin(t = 8, r = 8, b = 8, l = 8)))
+}
+
+theme_set(theme_minimal() + lucify_basics())
+set.seed(51295)
+
+muestra_normal <- 
+
